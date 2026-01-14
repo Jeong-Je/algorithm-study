3876de453149d039fe6853451418ce1d2bd829ad
SON JEONG JE <jeongjeson656@gmail.com>
2026-01-14 09:59:41 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+vector<bool> isUsed(10);
+vector<int> ans;
+
+void func(int start, int cnt);
+
+int main() {
+    cin >> n >> m;
+    v.resize(n);
+
+    for (int i=0;i<n;i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    func(0, 0);
+}
+
+
+void func(int start, int cnt) {
+    if (cnt == m) {
+        for (int a: ans) {
+            cout << a << ' ';
+        }
+        cout << '\n';
+    }
+
+    for (int i=start;i<n;i++) {
+        if (isUsed[i]) continue;
+
+        isUsed[i] = true;
+        ans.push_back(v[i]);
+        func(i+1, cnt + 1);
+        isUsed[i] = false;
+        ans.pop_back();
+    }
+}
