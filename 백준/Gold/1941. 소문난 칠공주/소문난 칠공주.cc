efc6567f80d963d074e483c7e8570a27251c92be
SON JEONG JE <jeongjeson656@gmail.com>
2026-01-14 21:26:45 +0900

    [Gold III] Title: 소문난 칠공주, Time: 92 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -3,90 +3,87 @@
 
 using namespace std;
 
-static char arr[5][5];
-static bool visited[5][5];
-static bool chosen[25];
-static int result;
+char map[5][5];
+int chk[5][5];
+bool vis[5][5];
+int ans;
+int dr[4] = {-1, 1, 0, 0};
+int dc[4] = {0, 0, -1, 1};
 
-int di[4] = {1, -1, 0, 0};
-int dj[4] = {0, 0, -1, 1};
+void func(int start, int cnt);
+int cor[7];
 
-void DFS(int start, int count);
-bool isValid();
+bool bfs(int r, int c);
 
-int main(){
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
-            cin >> arr[i][j];
+int main() {
+    for (int i=0;i<5;i++) {
+        for (int j=0;j<5;j++) {
+            cin >> map[i][j];
         }
     }
-    
-    
-    DFS(0, 0);
-   
 
-    cout << result;
+    func(0, 0);
+
+    cout << ans;
 }
 
-void DFS(int start, int count) {
-    if(count == 7) { // 7명 결성 완료 
-        if(isValid()) result++; // 다솜파 4명 이상인지 + 상하좌우 연결되었는지
-    }
-    
-    for(int i=start;i<25;i++){ // 25개의 자리 중 7개를 조합
-        if(chosen[i]) continue;
-        chosen[i] = true;
-        DFS(i, count + 1);
-        chosen[i] = false;
-    }
-}
 
-bool isValid() {
-    int dasom_cnt = 0;
-    int connect_cnt = 0; 
-    fill(visited[0], visited[5], false);
-    
-    queue<pair<int,int>> q;
-    
-    bool isStartPoint = true;
-    for(int i=0;i<25;i++){
-        if(chosen[i] == true) {
-           int row = i / 5;
-           int column = i % 5;
-           
-           if(arr[row][column] == 'S') dasom_cnt++;
-           
-           if(isStartPoint == true) { // BFS 시작점
-               visited[row][column] = true;
-               q.push({row, column});
-               isStartPoint = false;
-               connect_cnt = 1;
-           }
+void func(int start, int cnt) {
+    // 7좌표를 모두 선택 완료 하였으면
+    if (cnt == 7) {
+        fill(chk[0], chk[5], 0);
+        fill(vis[0], vis[5], false);
+        int r, c;
+        for (int idx : cor) {
+            chk[idx/5][idx%5] = 1;
+            r = idx/5;
+            c = idx%5;
         }
+
+        if (bfs(r, c)) ans++;
+
+
+        return;
     }
-    
-    if(dasom_cnt < 4) return false;  // 다솜파 4명 미만 
-    
-    while(!q.empty()){
-        auto [now_row, now_column] = q.front();
+
+    for (int i=start;i<25;i++) {
+        cor[cnt] = i;
+        func(i+1, cnt + 1);
+    }
+}
+
+
+bool bfs(int r, int c) {
+    int cntY = 0, cntS = 0;
+    if (map[r][c] == 'Y') cntY++;
+    else cntS++;
+
+    queue<pair<int, int>> q;
+    q.push({r, c});
+    vis[r][c] = true;
+
+    while (!q.empty()) {
+        auto [nowR, nowC] = q.front();
         q.pop();
-        
-        for(int d=0;d<4;d++){
-            int next_row = now_row + di[d];
-            int next_column = now_column + dj[d];
-            
-            if(next_column >= 0 && next_column < 5 && next_row >= 0 && next_row < 5) {
-                if(visited[next_row][next_column] == 0) {
-                    if(chosen[next_row * 5 + next_column] == true) {
-                        visited[next_row][next_column] = true;
-                        q.push({next_row, next_column});
-                        connect_cnt++;
-                    }
-                }
-            }
+
+        for (int d=0;d<4;d++) {
+            int nexR = nowR + dr[d];
+            int nexC = nowC + dc[d];
+
+            if (nexR < 0 || nexR >= 5 || nexC < 0 || nexC >= 5) continue;
+            if (chk[nexR][nexC] != 1) continue;
+            if (vis[nexR][nexC]) continue;
+
+            if (map[nexR][nexC] == 'S') cntS++;
+            else cntY++;
+
+            vis[nexR][nexC] = true;
+            q.push({nexR, nexC});
         }
     }
-    
-    if(connect_cnt == 7) return true;
-    return false;
+
+    if (cntS + cntY != 7) return false;
+    if (cntS < 4) return false;
+
+    return true;
 }
